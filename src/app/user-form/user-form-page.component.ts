ca4c8be6e0b7371a9f7bc4c43d9fc2979ed73d03
Ajay <ajay.r@namosthuthe.com>
2018-10-23 22:01:00 +0530

    all changes

--- src/app/user-form/user-form-page.component.ts
+++ src/app/user-form/user-form-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms'; 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-user-form--page',
   templateUrl: './user-form-page.component.html',
@@ -18,7 +20,7 @@ export class UserFormPageComponent implements OnInit {
   Blog:string='';  
   Email:string='';  
   IsAccepted:number=0; 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder , private http:HttpClient) { 
     this.regiForm = fb.group({  
       'FirstName' : [null, Validators.required],  
       'Address' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],  
@@ -31,16 +33,41 @@ export class UserFormPageComponent implements OnInit {
 
   ngOnChanges() {
     console.log(this.userData);
+    if(this.userData){
+      this.regiForm.patchValue({
+       
+        FirstName:this.userData.result.name, 
+        Address:this.userData.result.address, 
+        Email:this.userData.result.email, 
+      
+      })
+    }
+
   }
   onChange(event:any)  
   {  
 
   }  
+
+  updateUser(id){
+    let data = {
+       
+      FirstName:this.userData.FirstName, 
+      Address:this.userData.Address, 
+      Email:this.userData.Email, 
+    
+    }
+    this.http.put('https://gorest.co.in/public-api/users/'+id+
+    '?access-token=07J57KaczxBrx6q2AUrFexhcFs7e9rzjJkv7',data).subscribe(data => {
+      location.reload();
+    });
+  }
+
   
   // Executed When Form Is Submitted  
   onFormSubmit(form:NgForm)  
   {  
-    console.log(form);  
+     
   }  
 
 }
--- src/app/user-list/user-list-page.component.ts
+++ src/app/user-list/user-list-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  , Input} from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 import { element } from 'protractor';
 import { NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms'; 
 
 export interface PeriodicElement {
   name: string;
@@ -11,6 +12,8 @@ export interface PeriodicElement {
   symbol: string;
   actions:any;
 }
+
+
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H',actions:"" },
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He',actions:""},
@@ -28,11 +31,26 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 
 export class UserListPageComponent implements OnInit {
+  regiForm: FormGroup;  
+  FirstName:string='';   
+  Address:string='';  
+  Email:string='';  
+  Id:number;
+  addButton = false;
+  editButton = false;
 private dataSource:any;
 userFormVisibility:boolean = false;
 show_form_page:boolean = false;
 userData:any;
-  constructor(private http:HttpClient) { }
+constructor(private fb: FormBuilder , private http:HttpClient) { 
+  this.regiForm = fb.group({  
+    'FirstName' : [null, Validators.required],  
+  
+    'Address' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],  
+    'Email':[null, Validators.compose([Validators.required,Validators.email])],
+    'Id':[null,null]
+  });  
+}
 
   ngOnInit() {
     this.http.get('https://gorest.co.in/public-api/users?_format=json').subscribe(data => {
@@ -45,25 +63,62 @@ userData:any;
     this.loadUserForm();
     this.http.get('https://gorest.co.in/public-api/users/'+id+'?access-token=07J57KaczxBrx6q2AUrFexhcFs7e9rzjJkv7').subscribe(data => {
       this.userData = data;
+      this.regiForm.patchValue({
+        Id:id,
+        FirstName:this.userData.result.name, 
+        Address:this.userData.result.address, 
+        Email:this.userData.result.email, 
+      })
     });
     
   }
+
   deleteUserDetails(id){
     this.http.delete('https://gorest.co.in/public-api/users/'+id+'?access-token=07J57KaczxBrx6q2AUrFexhcFs7e9rzjJkv7').subscribe(data => {
       location.reload();
     });
   }
 
+  updateUser(){
+    let data ={
+      "email": this.regiForm.value.Email,
+      "gender": "female",
+      "first_name": this.regiForm.value.FirstName,
+      "last_name": this.regiForm.value.FirstName,
+      "address":this.regiForm.value.Address
+      
+    }
+    this.http.put('https://gorest.co.in/public-api/users/'+this.regiForm.value.Id+'?access-token=07J57KaczxBrx6q2AUrFexhcFs7e9rzjJkv7',data).subscribe(data => {
+      location.reload();
+    });
+  }
+
+  addUser(){
+    let data ={
+      "email": this.regiForm.value.Email,
+      "gender": "female",
+      "first_name": this.regiForm.value.FirstName,
+      "last_name": this.regiForm.value.FirstName,
+      "address":this.regiForm.value.Address
+      
+    }
+    
+    this.http.post('https://gorest.co.in/public-api/users?access-token=07J57KaczxBrx6q2AUrFexhcFs7e9rzjJkv7',data).subscribe(data => {
+      location.reload();
+    });
+  }
   displayedColumns: string[] = ['id', 'name', 'address', 'email', 'status'];
 
   loadUserForm(){
     this.show_form_page = !this.show_form_page;
-    if(this.userFormVisibility){
-      this.userFormVisibility = false;
-    } else {
-      this.userFormVisibility = true;
-    }
-    
+    this.addButton = false;
+    this.editButton = true;
+  }
+
+  loadUserFormAdd(){
+    this.show_form_page = !this.show_form_page;
+    this.addButton = true;
+    this.editButton = false;
   }
 
 }
